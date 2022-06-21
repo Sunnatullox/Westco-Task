af773596c4a633ed7845d7fe829a69d8dd5139a9
Sunnatullox <sunnatullox83@gmail.com>
2022-06-22 03:59:28 +0500

    Api Swagger documentation

--- routes/courseLesson.js
+++ routes/courseLesson.js
@@ -48,11 +48,11 @@ router.put("/admin/lesson/update/:id", Auth, async (req, res, next) => {
     lesson?.mv(path.resolve(__dirname, "..", "CourseFile", file))
 
     try {
-        const lessonUpdate = await CourseLesson.findByIdAndUpdate({ _id: id, },
-            { $set: { name, lesson: file, description, } })
-        console.log(lessonUpdate)
-        return res.status(200).json({ msg: "kurs vuvafaqiyatli o'zgartirildi" })
+        await CourseLesson.findByIdAndUpdate({ _id: id, },
+            { $set: { name, lesson: file, description, } }, { new: true })
+        return res.status(200).json({ msg: "kurs muvafaqiyatli o'zgartirildi" })
     } catch (error) {
+
         return res.status(500).json({ error: "kechirasiz darslar ozgartirilmadi nimadir hato ketdi!" })
     }
 })
@@ -68,7 +68,7 @@ router.delete("/admin/lesson/delete/:id", Auth, async (req, res) => {
     }
 })
 
-router.post("/admin/courseBooks/create",Auth, async (req, res) => {
+router.post("/admin/courseBooks/create", Auth, async (req, res) => {
     const { courseId, name, urlBooks, description } = req.body;
     const coursBooks = req.files?.books;
     const filename = coursBooks.name
@@ -88,26 +88,27 @@ router.post("/admin/courseBooks/create",Auth, async (req, res) => {
 })
 
 
-router.put("/admin/coursBooks/update/:id",Auth, async (req, res) => {
+router.put("/admin/coursBooks/update/:id", Auth, async (req, res) => {
     const { id } = req.params;
     const { name, urlBooks, description } = req.body;
     const coursBooks = req.files?.books
+    const fileName = coursBooks?.name
     try {
-        const file = coursBooks && uuid.v4() + "-file" + ".jpg"
+        const file = coursBooks && uuid.v4() + "-file" + fileName
         coursBooks?.mv(path.resolve(__dirname, "..", "CourseFile", file))
 
-        await CourseBooks.findByIdAndUpdate({_id: id}, 
-            {$set : {name, urlBooks, description, books:file}})
-            return res.status(200).json({msg:"Kitob o'zgartitildi"})
+        await CourseBooks.findByIdAndUpdate({ _id: id },
+            { $set: { name, urlBooks, description, books: file } })
+        return res.status(200).json({ msg: "Kitob o'zgartitildi" })
     } catch (error) {
         return res.status(500).json({ error: "ktobni o'zgartirib bo'lmadi serverda hatolik!" })
     }
 })
-router.delete("/admin/coursBooks/delete/:id",Auth, async (req, res) => {
+router.delete("/admin/coursBooks/delete/:id", Auth, async (req, res) => {
     const { id } = req.params;
     try {
-         await CourseBooks.findByIdAndDelete({_id: id})
-            return res.status(200).json({msg:"Kitob o'chirildi"})
+        await CourseBooks.findByIdAndDelete({ _id: id })
+        return res.status(200).json({ msg: "Kitob o'chirildi" })
     } catch (error) {
         return res.status(500).json({ error: "ktobni o'zgartirib bo'lmadi serverda hatolik!" })
     }
